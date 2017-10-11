461125a614dfcff9d6a2363f43f954125029170c
github username <github email address>
2017-10-11 16:48:04 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
+  output_array = []
+  array.each do |elmt|
+    square = elmt ** 2
+    output_array << square
+  end
+  return output_array
   # your code here
 end
+
+# puts square_array([1,2,3,4,5])
